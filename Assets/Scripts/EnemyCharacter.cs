c7aa4c0f38cdba76ce7666eeb91d4fa93696e58d
Orlov <dvaoru@gmail.com>
2025-10-26 20:08:23 +0300

    Сделал сглаживание поворотов

--- Assets/Scripts/EnemyCharacter.cs
+++ Assets/Scripts/EnemyCharacter.cs
@@ -6,6 +6,9 @@ public class EnemyCharacter : Character
     public Vector3 targetPosition { get; private set; } = Vector3.zero;
     private float _velocityMagnitude = 0;
 
+    private RotatePredictor _rotatePredictorX = new RotatePredictor();
+    private RotatePredictor _rotatePredictorY = new RotatePredictor();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,16 +21,15 @@ public class EnemyCharacter : Character
         if (_velocityMagnitude > .1f)
         {
             float maxDistance = _velocityMagnitude * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                targetPosition,
-                maxDistance
-            );
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, maxDistance);
         }
         else
         {
             transform.position = targetPosition;
         }
+
+        _head.localEulerAngles = new Vector3(_rotatePredictorX.GetTargetRotation(_head.localEulerAngles.x), 0, 0);
+        transform.localEulerAngles = new Vector3(0, _rotatePredictorY.GetTargetRotation(transform.localEulerAngles.y), 0);
     }
 
     public void SetSpeed(float value)
@@ -45,11 +47,45 @@ public class EnemyCharacter : Character
 
     public void SetRotateX(float value)
     {
-        _head.localEulerAngles = new Vector3(value, 0, 0);
+        Debug.Log("Set rotate X " + Time.time + " " + value);
+        // _head.localEulerAngles = new Vector3(value, 0, 0);
+        _rotatePredictorX.SetRotate(value);
     }
-    
+
     public void SetRotateY(float value)
     {
-        transform.localEulerAngles = new Vector3(0, value, 0);
+        Debug.Log("Set rotate Y " + Time.time + " " + value);
+        _rotatePredictorY.SetRotate(value);
+        //transform.localEulerAngles = new Vector3(0, value, 0);
+    }
+
+    private class RotatePredictor
+    {
+        private float _lastRotate;
+        private float _lastTime;
+        private float _angularSpeed = 0;
+
+        public void SetRotate(float currentRotation)
+        {
+            float time = Time.time;
+
+            float deltaTime = time - _lastTime;
+            if (deltaTime > 0.01f)
+            {
+                float deltaAngle = Mathf.DeltaAngle(_lastRotate, currentRotation);
+                _angularSpeed = deltaAngle / deltaTime; //Расчитываем угловую скорость при повороте от предыдущего значения
+            }
+
+            _lastRotate = currentRotation;
+            _lastTime = time;
+        }
+
+        public float GetTargetRotation(float currentRotation)
+        {
+            var predictionTime = 0.1f; //Предсказываем поворот на 100 мс
+            float smoothSpeed = 180f; //Для сглаживания делаем скорость поворота 180 град/сек
+            float predicted = _lastRotate + _angularSpeed * predictionTime;            
+            return Mathf.MoveTowardsAngle(currentRotation, predicted, smoothSpeed * Time.deltaTime);
+        }
     }
 }
